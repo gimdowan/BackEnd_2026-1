11446f218eeef81825bceba14b7eb3b07aa40b9f
김도완 <ehdhksf1@gmail.com>
2026-05-13 20:51:18 +0900

    클래스 수정및 introduce 반환

--- src/main/java/com/example/demo/MyController.java
+++ src/main/java/com/example/demo/MyController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class HelloController {
+public class MyController {
 
     @ResponseBody
     @GetMapping("/hello")
@@ -13,8 +13,8 @@ public class HelloController {
         return "Hello World!";
     }
 
-    @GetMapping("/hello2")
-    public String hello2() {
-        return "hello";
+    @GetMapping("/introduce")
+    public String introduce() {
+        return "introduce";
     }
 }
