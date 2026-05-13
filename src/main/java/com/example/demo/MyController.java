c5184210d557e753d37b10f97798e0b8efdc3c1d
김도완 <ehdhksf1@gmail.com>
2026-05-13 21:07:12 +0900

    requestparam 반환

--- src/main/java/com/example/demo/MyController.java
+++ src/main/java/com/example/demo/MyController.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MyController {
@@ -12,9 +13,9 @@ public class MyController {
     public String hello() {
         return "Hello World!";
     }
-
+    @ResponseBody
     @GetMapping("/introduce")
-    public String introduce() {
-        return "introduce";
+    public String introduce(@RequestParam String name) {
+        return "안녕하세요 제 이름은 " + name + "입니다!";
     }
 }
