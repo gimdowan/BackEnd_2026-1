d78966338b3ed3f4e9397a7fd95a01a08c547539
김도완 <ehdhksf1@gmail.com>
2026-05-15 19:47:53 +0900

    json 기능 추가

--- src/main/java/com/example/demo/MyController.java
+++ src/main/java/com/example/demo/MyController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import java.util.Map;
+import java.util.HashMap;
 
 @Controller
 public class MyController {
@@ -17,5 +19,16 @@ public class MyController {
     @GetMapping("/introduce")
     public String introduce(@RequestParam String name) {
         return "안녕하세요 제 이름은 " + name + "입니다!";
+    }
+    @ResponseBody
+    @GetMapping("/json")
+    public Map<String, Object> json() {
+
+        Map<String, Object> map = new HashMap<>();
+
+        map.put("age", 23);
+        map.put("name", "김도완");
+
+        return map;
     }
 }
